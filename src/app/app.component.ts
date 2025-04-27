e09feb3a470aaccfebbf6c13a0358323836ba2f3
leonkae <leon29kae@gmail.com>
2025-04-27 11:30:41 +0300

    modify: app components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,6 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'rocket-bunny-update';
+  title = 'Welcome to Rocket bunny';
 }
+
